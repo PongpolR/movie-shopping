e6d27d3d76a396b93ce548f5285f2e61a06eeea3
PongpolR <pongpol.yy55@gmail.com>
2023-08-26 02:05:27 +0700

    add price property

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,20 +1,35 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import price from "../data/price";
 
 export default function Data() {
   const [movie, setMovie] = useState(
     JSON.parse(localStorage.getItem("movie")) || []
   );
-  const [searchMovie, setSearchMovie] = useState(
-    JSON.parse(localStorage.getItem("movie")) || []
-  );
+  const [searchMovie, setSearchMovie] = useState([]);
   const [search, setSearch] = useState("");
+  const [priceItem, setPriceItem] = useState(price);
   async function fetchData() {
     let response = await axios(
       "https://api.themoviedb.org/3/search/movie?api_key=ac70bed77550b024ac4d0cd98fa1df92&query=a"
     );
     let data = response.data;
     setMovie(data.results);
+    // localStorage.setItem("movie", JSON.stringify(movie));
+    // console.log(movie);
+    setPrice();
+  }
+
+  function setPrice() {
+    console.log(priceItem);
+    let objWithPrice = movie.map((item, id) => {
+      item = {
+        ...item,
+        price: priceItem[id].price,
+      };
+      return item;
+    });
+    setMovie(objWithPrice);
   }
 
   useEffect(() => {
@@ -27,12 +42,11 @@ export default function Data() {
     const result = movie.filter((item) =>
       item.title.toLowerCase().includes(search)
     );
-    console.log(result);
     setSearch("");
     setSearchMovie(result);
   }
 
-  function clear(e) {
+  function refresh(e) {
     e.preventDefault();
     setSearchMovie(movie);
   }
@@ -46,12 +60,25 @@ export default function Data() {
           onChange={(e) => setSearch(e.target.value)}
         />
         <button type="submit">Submit</button>
-        <button onClick={clear}>Clear</button>
+        <button onClick={refresh}>Refresh</button>
       </form>
 
       {searchMovie.map((item) => {
-        return <div key={item.id}>{item.title}</div>;
+        return (
+          <div key={item.id}>
+            {item.title} {item.price}
+          </div>
+        );
       })}
+
+      {searchMovie == 0 &&
+        movie.map((item) => {
+          return (
+            <div key={item.id}>
+              {item.title} {item.price}
+            </div>
+          );
+        })}
     </>
   );
 }
---
+++ src/data/price.jsx
@@ -0,0 +1,24 @@
+const price = [
+  { id: 1, price: 590 },
+  { id: 2, price: 450 },
+  { id: 3, price: 299 },
+  { id: 4, price: 299 },
+  { id: 5, price: 399 },
+  { id: 6, price: 299 },
+  { id: 7, price: 299 },
+  { id: 8, price: 299 },
+  { id: 9, price: 299 },
+  { id: 10, price: 399 },
+  { id: 11, price: 299 },
+  { id: 12, price: 299 },
+  { id: 13, price: 299 },
+  { id: 14, price: 399 },
+  { id: 15, price: 299 },
+  { id: 16, price: 399 },
+  { id: 17, price: 299 },
+  { id: 18, price: 399 },
+  { id: 19, price: 299 },
+  { id: 20, price: 419 },
+];
+
+export default price;
